3bc458f8ad08d8c966a446f81741aff90862ce73
Chandradeep <chandradeep_chandra@yahoo.com>
2017-08-03 00:40:54 +0530

    Added Exception handling to Hindu_URL_Extractor function in TH_functions.py

--- TH_functions.py
+++ TH_functions.py
@@ -16,12 +16,37 @@ def Hindu_URL_Extractor(URL):
 	Author
 	Date time
 	"""
-	title =BeautifulSoup(urllib2.urlopen(URL)).find_all("h1",{"class":re.compile("title")})[0].text.strip().encode('utf-8')
-	caption =BeautifulSoup(urllib2.urlopen(URL)).find_all("h2",{"class":re.compile("intro")})[0].text.strip().encode('utf-8')
-	body =BeautifulSoup(urllib2.urlopen(URL)).find_all("div",{"id":re.compile("content-body-*")})[0].text.strip().encode('utf-8')
-	imgUrl =re.sub('FREE_215','FREE_660',BeautifulSoup(urllib2.urlopen(URL)).find_all("img",{"class":re.compile("media-object adaptive placeholder lead-img")})[0]['data-proxy-image'])
-	author = BeautifulSoup(urllib2.urlopen(URL)).find_all("a",{"class":re.compile("auth-nm lnk|auth-nm no-lnk")})[0].text.strip().encode('utf-8')
-	tmstmp = BeautifulSoup(urllib2.urlopen(URL)).find_all("div",{"class":re.compile("ut-container")})[0].text.strip().encode('utf-8')
+	try:
+		title =BeautifulSoup(urllib2.urlopen(URL)).find_all("h1",{"class":re.compile("title")})[0].text.strip().encode('utf-8')
+	except IndexError:
+		title = ''
+
+	try:
+		caption =BeautifulSoup(urllib2.urlopen(URL)).find_all("h2",{"class":re.compile("intro")})[0].text.strip().encode('utf-8')
+	except IndexError:
+		caption = ''
+
+	try:
+		body =BeautifulSoup(urllib2.urlopen(URL)).find_all("div",{"id":re.compile("content-body-*")})[0].text.strip().encode('utf-8')
+	except IndexError:
+		body = ''
+
+	try:
+		imgUrl =re.sub('FREE_215','FREE_660',BeautifulSoup(urllib2.urlopen(URL)).find_all("img",{"class":re.compile("media-object adaptive placeholder lead-img")})[0]['data-proxy-image'])
+	except IndexError:
+		imgUrl = ''
+
+	try:
+		author = BeautifulSoup(urllib2.urlopen(URL)).find_all("a",{"class":re.compile("auth-nm lnk|auth-nm no-lnk")})[0].text.strip().encode('utf-8')
+	except IndexError:
+		author = ''
+
+	try:
+		tmstmp = BeautifulSoup(urllib2.urlopen(URL)).find_all("div",{"class":re.compile("ut-container")})[0].text.strip().encode('utf-8')
+	except IndexError:
+		tmstmp = ''
+
+	
 	row = {}
 	row["Title"] = title
 	row["Caption"] = caption
