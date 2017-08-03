657d9e53b1f3153e6f22fc956c881b29da6cc2b7
Chandradeep <chandradeep_chandra@yahoo.com>
2017-08-03 17:34:28 +0530

    Small changes to the Hindu_article_content_extractor function' arguments

--- TH_functions.py
+++ TH_functions.py
@@ -6,7 +6,7 @@ from datetime import date, timedelta
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
 
-def TH_Article_Content_Extractor(DayURL):
+def TH_Article_Content_Extractor(ArtURL):
 	""" 
 	This function takes in a hindu article url and 
 	returns a dictionary with following details:
@@ -18,32 +18,32 @@ def TH_Article_Content_Extractor(DayURL):
 	Date time
 	"""
 	try:
-		title =BeautifulSoup(urllib2.urlopen(DayURL)).find_all("h1",{"class":re.compile("title")})[0].text.strip().encode('utf-8')
+		title =BeautifulSoup(urllib2.urlopen(ArtURL)).find_all("h1",{"class":re.compile("title")})[0].text.strip().encode('utf-8')
 	except IndexError:
 		title = ''
 
 	try:
-		caption =BeautifulSoup(urllib2.urlopen(DayURL)).find_all("h2",{"class":re.compile("intro")})[0].text.strip().encode('utf-8')
+		caption =BeautifulSoup(urllib2.urlopen(ArtURL)).find_all("h2",{"class":re.compile("intro")})[0].text.strip().encode('utf-8')
 	except IndexError:
 		caption = ''
 
 	try:
-		body =BeautifulSoup(urllib2.urlopen(DayURL)).find_all("div",{"id":re.compile("content-body-*")})[0].text.strip().encode('utf-8')
+		body =BeautifulSoup(urllib2.urlopen(ArtURL)).find_all("div",{"id":re.compile("content-body-*")})[0].text.strip().encode('utf-8')
 	except IndexError:
 		body = ''
 
 	try:
-		imgUrl =re.sub('FREE_215','FREE_660',BeautifulSoup(urllib2.urlopen(DayURL)).find_all("img",{"class":re.compile("media-object adaptive placeholder lead-img")})[0]['data-proxy-image'])
+		imgUrl =re.sub('FREE_215','FREE_660',BeautifulSoup(urllib2.urlopen(ArtURL)).find_all("img",{"class":re.compile("media-object adaptive placeholder lead-img")})[0]['data-proxy-image'])
 	except IndexError:
 		imgUrl = ''
 
 	try:
-		author = BeautifulSoup(urllib2.urlopen(DayURL)).find_all("a",{"class":re.compile("auth-nm lnk|auth-nm no-lnk")})[0].text.strip().encode('utf-8')
+		author = BeautifulSoup(urllib2.urlopen(ArtURL)).find_all("a",{"class":re.compile("auth-nm lnk|auth-nm no-lnk")})[0].text.strip().encode('utf-8')
 	except IndexError:
 		author = ''
 
 	try:
-		tmstmp = BeautifulSoup(urllib2.urlopen(DayURL)).find_all("div",{"class":re.compile("ut-container")})[0].text.strip().encode('utf-8')
+		tmstmp = BeautifulSoup(urllib2.urlopen(ArtURL)).find_all("div",{"class":re.compile("ut-container")})[0].text.strip().encode('utf-8')
 	except IndexError:
 		tmstmp = ''
 
