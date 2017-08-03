75550285cc27bcbaee43e39049c1bba48cb1cd4b
Chandradeep <chandradeep_chandra@yahoo.com>
2017-08-03 21:09:19 +0530

    test is working A OK

--- Test.py
+++ Test.py
@@ -3,13 +3,12 @@ from TH_functions import *
 
 DayUrls = TH_DayUrl_Generator()
 
-for i in DayUrls:
-	print i 
+articl_dict = TH_Article_URL__Extractor(DayUrls)
+
+for i in  articl_dict:
+	for j in  TH_Article_Content_Extractor(i):
+		print j + '------'+TH_Article_Content_Extractor(i)[j]
 
 
-urls = 'http://www.thehindu.com/todays-paper/tp-opinion/the-only-way-to-deal-with-the-chinese-is-directly/article19260841.ece'
-all_info = TH_Article_Content_Extractor(urls)
-
-
-for i in all_info:
-	 print i+" ----- " +all_info[i]
+# for i in all_info:
+# 	 print i+" ----- " +all_info[i]
