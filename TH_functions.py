fb2f87ac30b024a83192e985dcba1c1afc0278fb
Chandradeep <chandradeep_chandra@yahoo.com>
2017-08-03 17:30:43 +0530

    Renamed functions to start with TH and few other small changes. . checked before commit. . module running A OK.

--- TH_functions.py
+++ TH_functions.py
@@ -6,7 +6,7 @@ from datetime import date, timedelta
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
 
-def Hindu_Article_Extractor(URL):
+def TH_Article_Content_Extractor(DayURL):
 	""" 
 	This function takes in a hindu article url and 
 	returns a dictionary with following details:
@@ -18,32 +18,32 @@ def Hindu_Article_Extractor(URL):
 	Date time
 	"""
 	try:
-		title =BeautifulSoup(urllib2.urlopen(URL)).find_all("h1",{"class":re.compile("title")})[0].text.strip().encode('utf-8')
+		title =BeautifulSoup(urllib2.urlopen(DayURL)).find_all("h1",{"class":re.compile("title")})[0].text.strip().encode('utf-8')
 	except IndexError:
 		title = ''
 
 	try:
-		caption =BeautifulSoup(urllib2.urlopen(URL)).find_all("h2",{"class":re.compile("intro")})[0].text.strip().encode('utf-8')
+		caption =BeautifulSoup(urllib2.urlopen(DayURL)).find_all("h2",{"class":re.compile("intro")})[0].text.strip().encode('utf-8')
 	except IndexError:
 		caption = ''
 
 	try:
-		body =BeautifulSoup(urllib2.urlopen(URL)).find_all("div",{"id":re.compile("content-body-*")})[0].text.strip().encode('utf-8')
+		body =BeautifulSoup(urllib2.urlopen(DayURL)).find_all("div",{"id":re.compile("content-body-*")})[0].text.strip().encode('utf-8')
 	except IndexError:
 		body = ''
 
 	try:
-		imgUrl =re.sub('FREE_215','FREE_660',BeautifulSoup(urllib2.urlopen(URL)).find_all("img",{"class":re.compile("media-object adaptive placeholder lead-img")})[0]['data-proxy-image'])
+		imgUrl =re.sub('FREE_215','FREE_660',BeautifulSoup(urllib2.urlopen(DayURL)).find_all("img",{"class":re.compile("media-object adaptive placeholder lead-img")})[0]['data-proxy-image'])
 	except IndexError:
 		imgUrl = ''
 
 	try:
-		author = BeautifulSoup(urllib2.urlopen(URL)).find_all("a",{"class":re.compile("auth-nm lnk|auth-nm no-lnk")})[0].text.strip().encode('utf-8')
+		author = BeautifulSoup(urllib2.urlopen(DayURL)).find_all("a",{"class":re.compile("auth-nm lnk|auth-nm no-lnk")})[0].text.strip().encode('utf-8')
 	except IndexError:
 		author = ''
 
 	try:
-		tmstmp = BeautifulSoup(urllib2.urlopen(URL)).find_all("div",{"class":re.compile("ut-container")})[0].text.strip().encode('utf-8')
+		tmstmp = BeautifulSoup(urllib2.urlopen(DayURL)).find_all("div",{"class":re.compile("ut-container")})[0].text.strip().encode('utf-8')
 	except IndexError:
 		tmstmp = ''
 
@@ -57,7 +57,7 @@ def Hindu_Article_Extractor(URL):
 	row["DateTime"] = tmstmp
 	return row
 
-def Hindu_DayUrl_Generator():
+def TH_DayUrl_Generator():
 	print "Enter Start and End Date details"
 	start_year = int(raw_input("Enter the start date year: "))
 	start_month = int(raw_input("Enter the start date month: "))
--- Test.py
+++ Test.py
@@ -1,15 +1,15 @@
 from TH_functions import *
 
 
-DayUrls = Hindu_DayUrl_Generator()
+DayUrls = TH_DayUrl_Generator()
 
 for i in DayUrls:
 	print i 
 
 
-# urls = 'http://www.thehindu.com/todays-paper/tp-opinion/the-only-way-to-deal-with-the-chinese-is-directly/article19260841.ece'
-# all_info = Hindu_Article_Extractor(urls)
+urls = 'http://www.thehindu.com/todays-paper/tp-opinion/the-only-way-to-deal-with-the-chinese-is-directly/article19260841.ece'
+all_info = TH_Article_Content_Extractor(urls)
 
 
-# for i in all_info:
-# 	 print i+" ----- " +all_info[i]
+for i in all_info:
+	 print i+" ----- " +all_info[i]
